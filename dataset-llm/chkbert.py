0aca21ebad7582b99dc6c07173972880f4d78c56
Alexander Van Hecke <alexander.vanhecke@student.ou.nl>
2025-02-25 07:06:55 +0100

    remove unwanted keywords

--- dataset-llm/chkbert.py
+++ dataset-llm/chkbert.py
@@ -28,21 +28,27 @@ def extract(target_db: str, limit: int = 20000000, offset: int = 0):
         keyphrase_vectorizer: KeyphraseCountVectorizer = KeyphraseCountVectorizer(spacy_pipeline="en_core_web_trf")
         chunkey_bert: ChunkeyBert = ChunkeyBert(keybert=keybert)
 
+        unwanted_keywords = {"dutchnews", "dutch news"}
+        max_number_of_keywords = 5
         i = 1
         for r in conn.execute(f"select t.url, t.translated_text from hits_translation t left outer join hits_keywords k on t.url = k.url where k.url is null limit {limit} offset {offset}"):
             words = r[1].split(" ")
             print(f"summarizing {i}/{count}, word count {len(words)}:  {r[0]}")
             try:
                 result = chunkey_bert.extract_keywords(
-                    docs=r[1], num_keywords=5, chunker=chunker_leestekens, vectorizer=keyphrase_vectorizer, nr_candidates=10, top_n=3
+                    docs=r[1], num_keywords=10, chunker=chunker_leestekens, vectorizer=keyphrase_vectorizer, nr_candidates=10, top_n=3
                 )
             except Exception as e:
                 print(f"error summarizing {r[0]}")
-            keywords = set()
+
             if (len(result) > 0):
                 first_result = result[0]
+                keywords = set()
+                keywords_used = 0
                 for keyword, _ in first_result:
-                    keywords.add(str(keyword))
+                    if (str(keyword).lower() not in unwanted_keywords) and (keywords_used < max_number_of_keywords):
+                        keywords.add(str(keyword))
+                        keywords_used = keywords_used + 1
             keyword_set = ",".join(sorted(keywords))
             conn.execute("insert into hits_keywords (url, keywords) values (?, ?)", (r[0], keyword_set))
             conn.commit()
