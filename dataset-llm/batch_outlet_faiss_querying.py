c0d9bd59cbf8ef63170736511c719c267d902728
Alexander Van Hecke <alexander@avhconsult.be>
2025-03-04 16:47:01 +0100

    outlet batch querying

--- dataset-llm/batch_outlet_faiss_querying.py
+++ dataset-llm/batch_outlet_faiss_querying.py
@@ -1,31 +1,58 @@
 import json
 
+from faiss_utils import models_and_params, get_index_and_metadata, BASE_EMBEDDING_MODEL, MULTILINGUAL_EMBEDDING_MODEL
 from outlet_faiss_querying import *
 
 
-def query_index(index_file: str, metadata_file: str, questions_file: str, output_file:str, nr_of_hits: int = 5):
-    # model = load_model("sentence-transformers/all-MiniLM-L6-v2")
-    model = load_model("nomic-ai/nomic-embed-text-v2-moe", trust_remote_code=True, device="cpu")
-    index, chunk_metadata = read_index_and_metadata(index_file, metadata_file)
+KOLOM_NAMEN = ["translated_text", "content"]
 
-    responses = []
-    total_unique_retrieved_doc_ids = set()
+def query_index(faiss_folder: str, db_name: str, questions_file: str, nr_of_hits: int = 5):
+    queries = []
     with open(questions_file, "r") as f:
         for line in f:
             query = line.strip()
-            if not query:
-                continue
+            if query:
+                queries.append(query)
+    print(f"will execute {len(queries)} queries")
+    responses = {}
+    for query in queries:
+        responses[query] = {}
 
-            print(f"querying faiss index {index_file} with query {line}")
-            unique_retrieved_doc_ids = query_index_with_model(model, index, chunk_metadata, query, nr_of_hits)
-            responses.append({'query': query, 'retrieved_doc_ids': unique_retrieved_doc_ids})
-            total_unique_retrieved_doc_ids.update(unique_retrieved_doc_ids)
-    responses.append({'query': 'total', 'retrieved_doc_ids': list(total_unique_retrieved_doc_ids)})
-    with open(output_file, "w") as f:
+    for (model_name, dimension, kwargs) in models_and_params():
+        model = load_model(model_name, **kwargs)
+
+        total_unique_retrieved_doc_ids = set()
+        for column_name in KOLOM_NAMEN:
+            (index_path, metadata_path) = get_index_and_metadata(faiss_folder, model_name, column_name, db_name)
+            index, chunk_metadata = read_index_and_metadata(index_path, metadata_path)
+
+            for query in queries:
+                print(f"querying faiss index {index_path} with query {query}")
+                unique_retrieved_doc_ids = query_index_with_model(model, index, chunk_metadata, query, nr_of_hits)
+                responses[query].update({f'retrieved_doc_ids_{model_name}_{column_name}': unique_retrieved_doc_ids})
+                total_unique_retrieved_doc_ids.update(unique_retrieved_doc_ids)
+            # responses.append({'query': 'total', 'retrieved_doc_ids': list(total_unique_retrieved_doc_ids)})
+
+    for query in queries:
+        stats = responses[query]
+        # berekenen hoeveel intersectie er is tussen de verschillende modellen, op dezelfde kolommen
+        for column_name in KOLOM_NAMEN:
+            base_stats = stats[f"retrieved_doc_ids_{BASE_EMBEDDING_MODEL}_{column_name}"]
+            multilingual_stats = stats[f"retrieved_doc_ids_{MULTILINGUAL_EMBEDDING_MODEL}_{column_name}"]
+            common_for_column_name = list(set(base_stats).intersection(set(multilingual_stats)))
+            stats[f'intersection_{column_name}'] = common_for_column_name
+            stats[f'intersection_{column_name}_percentage'] = f"{(float(len(common_for_column_name)) / float(len(base_stats)) if len(base_stats) > 0 else 0) * 100:.2f}"
+        # berekenen hoeveel intersectie er is tussen de verschillende kolommen, op het nomic model
+        nomic_stats_first = stats[f"retrieved_doc_ids_{MULTILINGUAL_EMBEDDING_MODEL}_{KOLOM_NAMEN[0]}"]
+        nomic_stats_second = stats[f"retrieved_doc_ids_{MULTILINGUAL_EMBEDDING_MODEL}_{KOLOM_NAMEN[1]}"]
+        common_for_model = list(set(nomic_stats_first).intersection(set(nomic_stats_second)))
+        stats[f'intersection_cross_column_multilingual'] = common_for_model
+        stats[f'intersection_cross_column_multilingual_percentage'] = f"{(float(len(common_for_model)) / float(len(nomic_stats_first)) if len(base_stats) > 0 else 0) * 100:.2f}"
+    with open(f"{db_name[:-len('.sqlite')]}-{questions_file[:-len('.txt')]}.json", "w") as f:
         json.dump(responses, f)
 
 
 if __name__ == "__main__":
     if len(sys.argv) <= 3:
-        raise RuntimeError("usage : batch_outlet_faiss_querying.py <index_file> <metadata_file> <questions_file> <output_file> [nr_of_hits]")
-    query_index(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], int(sys.argv[5]) if len(sys.argv) > 5 else 5)
+        raise RuntimeError("usage : batch_outlet_faiss_querying.py <faiss_folder> <db_name.sqlite> <questions_file> [nr_of_hits]")
+    query_index(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]) if len(sys.argv) > 4 else 5)
--- dataset-llm/faiss_utils.py
+++ dataset-llm/faiss_utils.py
@@ -19,14 +19,23 @@ def index_name(model_name: str, column_name: str, db_name: str) -> str:
 
 
 def metadata_name(model_name: str, column_name: str, db_name: str) -> str:
-    return f"faiss_metadata_{clean_model_name(model_name)}_{column_name}_{normalized_db_name(db_name)[:-len('.sqlite')]}.bin"
+    return f"faiss_metadata_{clean_model_name(model_name)}_{column_name}_{normalized_db_name(db_name)[:-len('.sqlite')]}.npy"
 
+
+BASE_EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+MULTILINGUAL_EMBEDDING_MODEL = "nomic-ai/nomic-embed-text-v2-moe"
 
 def models_and_params():
-    return zip(["sentence-transformers/all-MiniLM-L6-v2", "nomic-ai/nomic-embed-text-v2-moe"], [384, 768], [{}, {"trust_remote_code": True, "device": "cpu"}])
+    return zip([BASE_EMBEDDING_MODEL, MULTILINGUAL_EMBEDDING_MODEL], [384, 768], [{}, {"trust_remote_code": True, "device": "cpu"}])
 
 
 MAX_WORDS = 250
 OVERLAP = 25
 def get_splitter(max_words: int = MAX_WORDS, overlap: int = OVERLAP) -> SentenceSplitter:
     return SentenceSplitter(chunk_size=max_words, chunk_overlap=overlap)
+
+
+def get_index_and_metadata(faiss_folder: str, model_name: str, column_name: str, db_name: str) -> (str, str):
+    index_path = os.path.join(faiss_folder, index_name(model_name, column_name, db_name))
+    metadata_path = os.path.join(faiss_folder, metadata_name(model_name, column_name, db_name))
+    return index_path, metadata_path
