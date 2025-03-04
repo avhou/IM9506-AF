6222a90bde19b563cdb4122022652d0329efc624
Alexander Van Hecke <alexander@avhconsult.be>
2025-03-04 12:21:51 +0100

    documenteren hoe de excel judgments in de sqlite versies te krijgen

--- dataset-llm/finalize_tiktok.py
+++ dataset-llm/finalize_tiktok.py
@@ -1,6 +1,11 @@
 import sys
 import duckdb
 
+# create table existing_judgments as (select * from read_xlsx('../database/tiktok_russia_ukraine_max_1_min.xlsx'));
+# update video_hits h set disinformation = (select e.disinformation from existing_judgments e where e.id = h.id);
+# update video_hits h set relevant = (select e.relevant from existing_judgments e where e.id = h.id);
+# drop table existing_judgments;
+
 def table_exists(conn, table_name) -> bool:
     result = conn.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';").fetchone()
     return result is not None
@@ -14,29 +19,33 @@ def finalize_tiktok(tiktok_db: str):
         conn.execute("load excel;")
         conn.execute(f"""ATTACH '{tiktok_db}' as tiktok (TYPE sqlite);""")
         for table, filename in zip(["videos_2nd_stage_optie_a", "videos_2nd_stage_optie_b", "long_videos_2nd_stage_optie_a", "long_videos_2nd_stage_optie_b"],
-                         ["tiktok_russia_ukraine_max_1_min.xlsx", "tiktok_more_wars_max_1_min.xlsx", "tiktok_russia_ukraine_max_2_min.xlsx", "tiktok_more_wars_max_2_min.xlsx"]):
+                         ["tiktok_russia_ukraine_max_1_min", "tiktok_more_wars_max_1_min", "tiktok_russia_ukraine_max_2_min", "tiktok_more_wars_max_2_min"]):
             print(f"processing table {table} and file {filename}")
             if table_exists(conn, table):
                 print(f"table {table} exists, will export now")
-                conn.execute(f"""
-                copy (
-                    select row_number() over() as number, 
-                           id::text as id, 
-                           video_description as description, 
-                           'https://www.tiktok.com/@' || user_name || '/video/' || id as url, 
-                           create_time, 
-                           video_duration, 
-                           comment_count, 
-                           transcription, 
-                           translated_text, 
-                           keywords,
-                           '' as relevant,
-                           '' as disinformation
-                from tiktok.{table}) 
-                to '{filename}' with (format xlsx, header true);""")
+                conn.execute(f"""copy({generate_query(table)}) to '{filename}.xlsx' with (format xlsx, header true);""")
+                conn.execute(f"""attach '{filename}.sqlite' as {filename} (TYPE sqlite);""")
+                conn.execute(f"""create table {filename}.video_hits as ({generate_query(table)});""")
             else:
                 print(f"table {table} does not exist, skipping")
 
+
+def generate_query(table: str) -> str:
+    return f"""
+                select row_number() over() as number, 
+                       id::text as id, 
+                       video_description as description, 
+                       'https://www.tiktok.com/@' || user_name || '/video/' || id as url, 
+                       create_time, 
+                       video_duration, 
+                       comment_count, 
+                       transcription, 
+                       translated_text, 
+                       keywords,
+                       '' as relevant,
+                       '' as disinformation
+            from tiktok.{table}
+            """
 
 
 
