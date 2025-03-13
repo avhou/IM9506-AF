0b4cacd9720fd8f39ab944e221d40036c5c06103
Alexander Van Hecke <alexander.vanhecke@student.ou.nl>
2025-03-13 20:53:55 +0100

    verfijningen reddit

--- dataset/reddit-concat.sh
+++ dataset/reddit-concat.sh
@@ -4,7 +4,9 @@ cat reddit*json | jq --sort-keys '.[] | . as $nested_objects | $nested_objects' 
 # create table expanded_comments as select id, comment->>'body' as body from (select id, unnest(comments) as comment from reddit);
 # delete from expanded_comments where body = '[deleted]' or body = '[removed]';
 # drop table if exists aggregated_comments;
-# create table aggregated_comments as select id, string_agg(body, ' ') as comments from expanded_comments group by id;
+# create table aggregated_comments as select id, regexp_replace(string_agg(body, ' '), '\\s+', ' ') as comments from expanded_comments group by id;
 # drop table if exists texts;
-# create table texts as select r.id, r.subreddit, to_timestamp(created_utc) as created, case when r.title in ('[deleted]', '[removed]') then '' else r.title end as title, case when r.selftext in ('[deleted]', '[removed]') then '' else r.selftext end as selftext, c.comments from reddit r left outer join aggregated_comments c on r.id = c.id;
+# create table texts as select r.id, r.subreddit, to_timestamp(created_utc) as created, case when r.title in ('[deleted]', '[removed]') then '' else regexp_replace(r.title, '\\s+', ' ') end as title, case when r.selftext in ('[deleted]', '[removed]') then '' else regexp_replace(r.selftext, '\\s+', ' ') end as selftext, c.comments from reddit r left outer join aggregated_comments c on r.id = c.id;
 # copy (select id, subreddit, created, trim(coalesce(title, '') || ' ' || coalesce(selftext, '') || ' ' || coalesce(comments, '')) as text from texts) to 'reddit.csv';
+# attach 'reddit.sqlite' as output (TYPE sqlite);
+# create table output.reddit_hits as select id, subreddit, created, trim(coalesce(title, '') || ' ' || coalesce(selftext, '') || ' ' || coalesce(comments, '')) as text from texts;
